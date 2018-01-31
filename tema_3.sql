087231f3c8aa012bef0c1a0b059100437dee8fb3
banica <banica@users.noreply.github.com>
2018-01-31 20:15:40 +0200

    Update tema_3.sql

--- tema_3.sql
+++ tema_3.sql
@@ -60,7 +60,7 @@ id_factura int(10) primary key auto_increment not null, /* id_factura */
 data_factura datetime not null default now(), /* data factura */
 valoare double(10,2), /*valoare factura */
 id_client int(10), FOREIGN KEY (id_client) REFERENCES clienti(id_client) /*clientul caruia i-a fost emisă factura */
-);
+); 
 
 /* 4. Scrieţi o instrucţiune SQL prin care să modificați numele tabelei facturi în facturi_clienti. */
 alter table facturi rename facturi_clienti;
